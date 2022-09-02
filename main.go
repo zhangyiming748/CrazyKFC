3ced1c5111b88a071d6362490f098bedde8032c3
zen <zhangyiming748@gmail.com>
2022-09-03 00:01:16 +0800

    termux单独处理

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -16,7 +17,12 @@ func main() {
 	d := thisYear() //今年
 	e := week()     //今天是今年的第几周
 	ret := communicate(a, b, c, d, e)
-	writeAll("/data/data/com.termux/files/home/storage/documents/report.txt", ret)
+	//
+	if runtime.GOOS == "android" {
+		writeAll("/data/data/com.termux/files/home/storage/documents/report.txt", ret)
+	} else {
+		writeAll("report.txt", ret)
+	}
 	fmt.Println(ret)
 
 }
@@ -56,12 +62,12 @@ func getWeek() (y, w int) {
 }
 
 func communicate(a, b, c, d, e string) string {
-	s := strings.Join([]string{"今天是", a, ",是我这个月零收入的第", b, "天,也是我今年零收入的第", c, "天,依旧没人带我赚大钱.外面又潮又冷,我很饿,身上的钱只够买一个馒头了,吃了这顿饭以后该怎么办?我很迷茫.以前电子厂一个月800非常的满足.每月还可以下两三次馆子,除去花销,有时候还能结余十多块.自从认识你们,就开始怀疑人生了,你们一谈就是一个月赚几万,看到你们晒的图,经常晚上睡不着,有时候甚至怀疑我们不是在同一个世界.大家在一个群里就是缘分,莫让错过成为你的遗憾,这星期四是KFC", d, "年第", e, "次疯狂星期四,V我50,拒绝遗憾.\n"}, "")
+	s := strings.Join([]string{"今天是", a, ",是我这个月零收入的第", b, "天,也是我今年零收入的第", c, "天,依旧没人带我赚大钱,外面又潮又冷,我很饿,身上的钱只够买一个馒头了,吃了这顿饭以后该怎么办?我很迷茫,以前电子厂一个月800非常的满足,每月还可以下两三次馆子,除去花销,有时候还能结余十多块,自从认识你们,就开始怀疑人生了,你们一谈就是一个月赚几万,看到你们晒的图,经常晚上睡不着,有时候甚至怀疑我们不是在同一个世界,大家在一个群里就是缘分,莫让错过成为你的遗憾,这星期四是KFC", d, "年第", e, "次疯狂星期四,V我50,拒绝遗憾\n"}, "")
 	return s
 }
 
 func writeAll(fname, content string) {
-	f, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0776)
+	f, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, 0776)
 	if err != nil {
 		log.Println(err)
 	}
